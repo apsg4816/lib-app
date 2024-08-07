a97cecb4e82904f50b17254382731f87fc5f06dc
21f1000465 <125444001+21f1000465@users.noreply.github.com>
2024-08-07 12:18:58 +0530

    Tried backend port change commit

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,7 @@ from celery_config import celery
 import redis
 from functools import wraps
 from flask_caching import Cache
+import os
 
 
 
@@ -688,5 +689,7 @@ def export_book_data():
         return jsonify({'error': str(e)}), 500
     
 
-if __name__ == '__main__':
-    app.run(port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
